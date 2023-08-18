96f2f5eb8c6ee1b8a2c7a9cf8d37c25cb29801a4
Giovanni Favorin de Melo <Giovannifavorim@gmail.com>
2023-08-18 17:29:27 -0300

    Initial Commit

---
+++ Nano3/Nano3App.swift
@@ -0,0 +1,17 @@
+//
+//  Nano3App.swift
+//  Nano3
+//
+//  Created by Giovanni Favorin de Melo on 18/08/23.
+//
+
+import SwiftUI
+
+@main
+struct Nano3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
