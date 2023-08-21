a764a4e0e3775c370154b34528060c6209e13123
kauamiguel <kauamcm@gmail.com>
2023-08-21 15:47:33 -0300

    Salvando o crud

--- Nano3/Persistence/PersistenceController.swift
+++ Nano3/Persistence/PersistenceController.swift
@@ -8,10 +8,11 @@
 import CloudKit
 import CoreData
 
-struct PersistenceController{
+class PersistenceController : ObservableObject{
     
     static let shared = PersistenceController()
     let container: NSPersistentCloudKitContainer
+    @Published var savedQuotes: [Banco] = []
     
     
     init(inMemory: Bool = false) {
@@ -28,6 +29,39 @@ struct PersistenceController{
         })
         
         container.viewContext.automaticallyMergesChangesFromParent = true
+    }
+    
+    func saveData(){
+        do{
+            try container.viewContext.save()
+            fetchQuotes()
+        }catch{
+            print("Error saving data")
+        }
+    }
+    
+    func fetchQuotes(){
+        let request = NSFetchRequest<Banco>(entityName: "Banco")
+        
+        do{
+            try savedQuotes = container.viewContext.fetch(request)
+        }catch{
+            print("Erro saving data")
+        }
+    }
+    
+    func addQuote(quote : Quotes){
+        let newQuote = Banco(context: container.viewContext)
+        newQuote.frase = quote.APIResponse.q
+        newQuote.autor = quote.APIResponse.a
+        newQuote.id = UUID()
+        saveData()
+    }
+    
+    func deleteObject(index : IndexSet){
+        let indexDeleted = index.first!
+        container.viewContext.delete(savedQuotes[indexDeleted])
+        saveData()
     }
     
 }
