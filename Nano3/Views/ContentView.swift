074e1582a68372be721c283b01792a6f2da2c4bc
useravsr <arthurreis120@gmail.com>
2023-08-21 15:45:54 -0300

    Views

---
+++ Nano3/Views/ContentView.swift
@@ -0,0 +1,40 @@
+//
+//  ContentView.swift
+//  FavFood
+//
+//  Created by Giovanni Favorin de Melo on 14/08/23.
+//
+
+import SwiftUI
+
+
+
+struct ContentView: View {
+    
+    @State var selectTab:Int = 0
+    
+    var body: some View {
+        TabView(selection: $selectTab){
+            NavigationView{
+                FrasesView()
+            }
+            .tabItem {
+                Label("Frases", systemImage: "book")
+            }
+            .tag(selectTab)
+            NavigationView{
+                FavoritosView()
+            }
+            .tabItem{
+                Label("Favoritos", systemImage: "star.fill")
+            }
+            .tag(selectTab)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Nano3/Views/FavoritosView.swift
@@ -0,0 +1,24 @@
+//
+//  FavoritosView.swift
+//  Nano3
+//
+//  Created by Arthur Dos Reis on 21/08/23.
+//
+
+import SwiftUI
+
+struct FavoritosView: View {
+    var body: some View {
+        VStack{
+            
+            
+        }
+        .navigationTitle("Favoritos")
+    }
+}
+
+struct FavoritosView_Previews: PreviewProvider {
+    static var previews: some View {
+        FavoritosView()
+    }
+}
