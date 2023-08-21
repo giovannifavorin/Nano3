2a1e7a73c889555feb5f3a580bfdaa7a4681d4d0
useravsr <arthurreis120@gmail.com>
2023-08-21 17:11:59 -0300

    Persistencia implementada

--- Nano3/Views/FavoritosView.swift
+++ Nano3/Views/FavoritosView.swift
@@ -8,8 +8,32 @@
 import SwiftUI
 
 struct FavoritosView: View {
+    
+    @Environment(\.managedObjectContext) private var moc 
+    @FetchRequest(entity: Banco.entity(), sortDescriptors: [])
+    private var acessoBanco:FetchedResults<Banco>
+    
+    @State private var acessoDelete: PersistenceController = PersistenceController()
+    
     var body: some View {
         VStack{
+            
+            List(){
+                ForEach(acessoBanco){ banco in
+                    VStack{
+                        HStack {
+                            Text("\(banco.autor ?? "Desconhecido")")
+                                .font(.headline)
+                            Spacer()
+                        }
+                        Text("\(banco.frase ?? "Desconhecida")")
+                    }
+                    
+                }
+                .onDelete{ offsets in
+                    acessoDelete.removeBanco(at: offsets, bancos: acessoBanco, moc: moc)
+                }
+            }
             
             
         }
