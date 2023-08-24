41497faca7526a015e45e720b0b5fc01ae841cbb
Eirado <GABEIRADO2@HOTMAIL.COm>
2023-08-24 01:32:07 -0300

    post/put/delete

--- Nano3/API/APIModel.swift
+++ Nano3/API/APIModel.swift
@@ -20,10 +20,10 @@ class APIModel{
         do {
             let (data, response) = try await URLSession.shared.data(from: url)
             
-            guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
+            guard let httpResponse = response as? HTTPURLResponse, (200...201).contains(httpResponse.statusCode)else {
                 throw APIError.invalidResponse("Invalid response: \(response)")
             }
-
+            
             let decoder = JSONDecoder()
             decoder.keyDecodingStrategy = .convertFromSnakeCase
             
@@ -39,7 +39,7 @@ class APIModel{
     }
     
     
-    func post<T: Decodable, U: Encodable>(APIurl: String, requestBody: U) async throws -> T {
+    func post<U: Encodable>(APIurl: String, body: U) async throws{
         
         guard let url = URL(string: APIurl) else {
             throw APIError.invalidURL
@@ -49,26 +49,85 @@ class APIModel{
             var request = URLRequest(url: url)
             request.httpMethod = "POST"
             request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-            request.httpBody = try JSONEncoder().encode(requestBody)
+            request.httpBody = try JSONEncoder().encode(body)
             
             let (data, response) = try await URLSession.shared.data(for: request)
             
-            guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
+            
+            guard let httpResponse = response as? HTTPURLResponse, (200...201).contains(httpResponse.statusCode) else {
                 throw APIError.invalidResponse("Invalid response: \(response)")
             }
+            print("HTTP Status Code: \(httpResponse.statusCode)")
             
-            let decoder = JSONDecoder()
-            decoder.keyDecodingStrategy = .convertFromSnakeCase
-            
-            do {
-                let decodedObject = try decoder.decode(T.self, from: data)
-                return decodedObject
-            } catch {
-                throw APIError.invalidData("Invalid data: \(error)")
+            if let dataResponse = String(data: data, encoding: .utf16) {
+                print("Response Data:\n\(dataResponse)")
             }
+            
         } catch {
             throw APIError.invalidData("Failed to fetch data: \(error)")
         }
     }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    func put<U: Encodable>(APIurl: String, body: U) async throws { // need more logic
+        
+        guard let url = URL(string: APIurl) else {
+            throw APIError.invalidURL
+        }
+        
+        do {
+            var request = URLRequest(url: url)
+            request.httpMethod = "PUT"
+            request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+            request.httpBody = try JSONEncoder().encode(body)
+            
+            let (data, response) = try await URLSession.shared.data(for: request)
+            
+            guard let httpResponse = response as? HTTPURLResponse, (200...204).contains(httpResponse.statusCode) else {
+                throw APIError.invalidResponse("Invalid response: \(response)")
+            }
+            print("HTTP Status Code: \(httpResponse.statusCode)")
+            
+            if let dataResponse = String(data: data, encoding: .utf8) {
+                print("Response Data:\n\(dataResponse)")
+            }
+            
+        } catch {
+            throw APIError.invalidData("Failed to update data: \(error)")
+        }
+    }
+    
+    func delete(APIurl: String) async throws { // need more logic
+        
+        guard let url = URL(string: APIurl) else {
+            throw APIError.invalidURL
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "DELETE"
+        
+        do {
+            let (_, response) = try await URLSession.shared.data(for: request)
+            
+            guard let httpResponse = response as? HTTPURLResponse, (200...204).contains(httpResponse.statusCode) else {
+                throw APIError.invalidResponse("Invalid response: \(response)")
+            }
+            print("HTTP Status Code: \(httpResponse.statusCode)")
+            
+        } catch {
+            throw APIError.invalidData("Failed to delete data: \(error)")
+        }
+    }
 }
+
 
