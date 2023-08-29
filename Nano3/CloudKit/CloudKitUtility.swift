d28fe6c4dc928d0797e63e0a4681d1fd1132acef
Eirado <GABEIRADO2@HOTMAIL.COm>
2023-08-29 10:20:38 -0300

    .

---
+++ Nano3/CloudKit/CloudKitUtility.swift
@@ -0,0 +1,8 @@
+//
+//  CloudKitUtility.swift
+//  Nano3
+//
+//  Created by Gabriel Eirado on 29/08/23.
+//
+
+import Foundation
---
+++ Nano3/CloudKit/CloudKitView.swift
@@ -0,0 +1,8 @@
+//
+//  CloudKitView.swift
+//  Nano3
+//
+//  Created by Gabriel Eirado on 29/08/23.
+//
+
+import Foundation
